a73330d29783887c79396a29d5a2a0b2103fe754
Emma Strubell <emma.strubell@gmail.com>
2018-01-27 16:06:20 -0500

    fix

--- network.py
+++ network.py
@@ -551,7 +551,7 @@ class Network(Configurable):
     print('UAS: %.2f    LAS: %.2f' % (uas, las))
     print(pos_correct_total, n_tokens)
     print('POS: %.2f' % (pos_correct_total/n_tokens)*100)
-    print('SRL acc: %.2f' % ((srl_correct_total / srl_count_total)*100))
+    print('SRL acc: %.2f' % (srl_correct_total / srl_count_total)*100)
     print('SRL F1: %.2f' % (overall_f1))
     return correct
   

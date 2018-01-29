c45a1ad438540541444838c1cd651b0e811cadea
Emma Strubell <emma.strubell@gmail.com>
2018-01-29 16:00:33 -0500

    fix dataset

--- dataset.py
+++ dataset.py
@@ -133,7 +133,7 @@ class Dataset(Configurable):
           else:
             head = int(head) - 1
           # srl_fields = [token[idx] if idx < len(token)-1 else 'O' for idx in range(srl_start_field, srl_start_field + sent_len)]
-          srl_fields = [token[idx] for idx in srl_take_indices] # todo can we use fancy indexing here?
+          srl_fields = [token[idx] for idx in srl_take_indices] + ['O'] * (sent_len - len(srl_take_indices)) # todo can we use fancy indexing here?
           srl_tags = [srls[s][0] for s in srl_fields]
           is_trigger = token[trigs.conll_idx] != '-' #np.any([s in self.trigger_indices for s in srl_tags])
           buff[i][j] = (word,) + words[word] + tags[tag1] + trigs[str(is_trigger)] + tags[tag2] + (head,) + rels[rel] + tuple(srl_tags)

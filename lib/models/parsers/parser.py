5b8a272008c4eaa7048b7e1bca2638a3b57eadf4
Emma Strubell <emma.strubell@gmail.com>
2018-01-30 19:02:55 -0500

    Fix

--- lib/models/parsers/parser.py
+++ lib/models/parsers/parser.py
@@ -90,7 +90,7 @@ class Parser(BaseParser):
 
     # maps joint predicate/pos indices to pos indices
     if self.joint_pos_predicates:
-      preds_to_pos_map = np.zeros([num_pred_classes, 1], dtype=tf.int32)
+      preds_to_pos_map = np.zeros([num_pred_classes, 1], dtype=int)
       for pred_label, pred_idx in vocabs[3].iteritems():
         pred, pos = pred_label.split('/')
         pos_idx = vocabs[1][pos]

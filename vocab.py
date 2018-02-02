ea00057b603fc0b713c8eaf6dbb83e8e53564066
Emma Strubell <emma.strubell@gmail.com>
2018-02-02 11:38:17 -0500

    dont add extra crap

--- vocab.py
+++ vocab.py
@@ -58,8 +58,8 @@ class Vocab(Configurable):
       self._cased = super(Vocab, self).cased
     else:
       self._cased = cased
-    if self.name == 'Tags':
-      self.SPECIAL_TOKENS = ('PAD', 'ROOT')
+    # if self.name == 'Tags':
+    #   self.SPECIAL_TOKENS = ('PAD', 'ROOT')
     elif self.name == 'Rels':
       self.SPECIAL_TOKENS = ('pad', self.root_label)
     elif self.name == 'Trigs':

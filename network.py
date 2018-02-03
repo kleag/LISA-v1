ea456398b49cc8b967945ca2f85924092df85899
Emma Strubell <emma.strubell@gmail.com>
2018-02-03 15:32:47 -0500

    fix

--- network.py
+++ network.py
@@ -522,6 +522,7 @@ class Network(Configurable):
           for i, (datum, word, pred) in enumerate(zip(data, words, preds)):
             pred = srl_preds_str[i] if srl_preds_str else []
             bio_pred = srl_preds[i] if np.any(srl_preds) else []
+            print(pred)
             word_str = word
             tag0_str = self.tags[pred[4]], # gold tag
             tag1_str = self.tags[pred[3]], # auto tag

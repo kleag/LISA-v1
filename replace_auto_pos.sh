e999952a3a7f9d8e3f2fed846ea813902cf5c500
Emma Strubell <emma.strubell@gmail.com>
2018-02-09 17:09:06 -0500

    pos script

--- replace_auto_pos.sh
+++ replace_auto_pos.sh
@@ -9,7 +9,7 @@ pred_pos_file=$2
 # orig file format:
 # nw/wsj/24/wsj_2437      0       0       Avions  NNP     NNS     7       compound        _       -       -       -       -       B-ORG   B-ARG0  O       O       (1
 
-paste <(awk '{print $1"\t"$2"\t"$3"\t"$4"}'  $orig_data_file) \
+paste <(awk '{print $1"\t"$2"\t"$3"\t"$4}'  $orig_data_file) \
       <(awk '{print $5}' $pred_pos_file ) \
       <(awk '{ s = ""; for (i = 6; i <= NF; i++) s = s $i "\t"; print s }' $orig_data_file)
 

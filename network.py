bd7d50d186132123db59b345228cbb7ab0164ba6
Emma Strubell <emma.strubell@gmail.com>
2018-02-12 18:32:38 -0500

    tune

--- network.py
+++ network.py
@@ -425,14 +425,14 @@ class Network(Configurable):
     attn_correct_counts = {}
     pos_correct_total = 0.
     n_tokens = 0.
+    if self.viterbi_decode:
+      print("Decoding w/ viterbi")
     for batch_num, (feed_dict, sents) in enumerate(minibatches()):
       mb_inputs = feed_dict[dataset.inputs]
       mb_targets = feed_dict[dataset.targets]
       forward_start = time.time()
       probs, n_cycles, len_2_cycles, srl_probs, srl_preds, srl_logits, srl_correct, srl_count, srl_trigger, srl_trigger_targets, transition_params, attn_weights, attn_correct, pos_correct, pos_preds = sess.run(op, feed_dict=feed_dict)
       forward_total_time += time.time() - forward_start
-      if self.viterbi_decode:
-        print("Decoding w/ viterbi")
       preds, parse_time, roots_lt, roots_gt, cycles_2, cycles_n, non_trees, non_tree_preds, n_tokens_batch = self.model.validate(mb_inputs, mb_targets, probs, n_cycles, len_2_cycles, srl_preds, srl_logits, srl_trigger, srl_trigger_targets, pos_preds, transition_params)
       n_tokens += n_tokens_batch
       for k, v in attn_weights.iteritems():

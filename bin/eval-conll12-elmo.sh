0f5448f7072a7c1937ae804001a8555c411ac899
Emma Strubell <emma.strubell@gmail.com>
2018-04-12 12:13:04 -0400

    eval

---
+++ bin/eval-conll12-elmo.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+#
+# Usage: ./eval-conll05.sh [model_dir] (regular|dm|gold)
+
+model_dir=$1
+parse_config=$2
+
+data_dir=$DATA_DIR/conll-2012-new
+
+valid_file=$data_dir/conll2012-dev.txt.bio
+valid_props_file=$data_dir/conll2012-dev-gold-props.txt
+valid_parse_file=$data_dir/conll2012-dev.conll
+
+# WSJ test
+test_file=$data_dir/conll2012-test.txt.bio
+test_props_file=$data_dir/conll2012-test-gold-props.txt
+test_parse_file=$data_dir/conll2012-test.conll
+
+# D&M injected
+# TODO need to train conll2012 elmo parsing model, get parse outputs then put them here
+dm_valid_file=lstm_predicted_parses/parse_preds-conll2012-lstm-dev.tsv.replaced
+dm_test_file=lstm_predicted_parses/parse_preds-conll2012-lstm-test.tsv.replaced
+
+if [[ "$parse_config" == "dm" || "$parse_config" == "gold" ]]; then
+    gold_attn_at_train="False"
+    inject_manual_attn="True"
+    if [[ "$parse_config" == "dm" ]]; then
+        echo "Doing D&M eval"
+        valid_file=$dm_valid_file
+        test_file=$test_file
+    else
+        echo "Doing Gold eval"
+    fi
+else
+    echo "Doing regular eval"
+    gold_attn_at_train="True"
+    inject_manual_attn="True"
+fi
+
+# Dev only
+python $DOZAT_ROOT/network.py \
+    --load \
+    --test \
+    --load_dir $model_dir \
+    --save_dir tmp \
+    --config_file $model_dir/config.cfg \
+    --gold_attn_at_train $gold_attn_at_train \
+    --inject_manual_attn $inject_manual_attn \
+    --valid_file $valid_file \
+    --test_file $test_file \
+    --gold_test_props_file $test_props_file \
+    --gold_dev_parse_file $valid_parse_file \
+    --gold_test_parse_file $test_parse_file
+#    --max_test_batch_size
+
+# WSJ test
+#python $DOZAT_ROOT/network.py \
+#    --load \
+#    --test \
+#    --test_eval \
+#    --load_dir $model_dir \
+#    --config_file $model_dir/config.cfg \
+#    --gold_attn_at_train $gold_attn_at_train \
+#    --inject_manual_attn $inject_manual_attn \
+#    --valid_file $valid_file \
+#    --test_file $wsj_test_file \
+#    --gold_test_props_file $wsj_test_props_file \
+#    --gold_dev_parse_file $valid_parse_file \
+#    --gold_test_parse_file $wsj_test_parse_file
+

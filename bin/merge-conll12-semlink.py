5078beafdad3914d6ec20bc1866af848a8f3038c
Emma Strubell <emma.strubell@gmail.com>
2018-04-23 16:07:22 -0400

    semlink

--- bin/merge-conll12-semlink.py
+++ bin/merge-conll12-semlink.py
@@ -35,7 +35,8 @@ with open(semlink_fname, 'r') as semlink_file:
           pb_arg, vn_arg = arg.split('=')
           if pb_arg not in arg_mappings:
             arg_mappings[pb_arg] = []
-          arg_mappings[pb_arg].append(vn_arg)
+          if vn_arg not in arg_mappings[pb_arg]:
+            arg_mappings[pb_arg].append(vn_arg)
 
       value = (split_line[7].split('.')[0], ' '.join(stripped_removed_args))
       if key not in semlink_map:

36164a1dfc95b2e28a3a2afd842c584eba1d8aa4
Emma Strubell <emma.strubell@gmail.com>
2018-04-30 17:54:33 -0400

    include sentences with no predicates

--- network.py
+++ network.py
@@ -435,6 +435,7 @@ class Network(Configurable):
         current_predicates = np.zeros(current_sent_shared.shape[0])
       current_srls.append(np.expand_dims(preds[:, -1], -1))
       if current_sent_shared.shape[1] > 14:
+        print(current_sent_shared)
         current_predicates += (preds[:, predicate_idx] > self._vocabs[4].predicate_true_start_idx).astype(np.int32)
         print("predicates", (preds[:, predicate_idx] > self._vocabs[4].predicate_true_start_idx).astype(np.int32))
 

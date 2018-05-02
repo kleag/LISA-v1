94054eed75c7b4dc1edf7821a405657e83e56645
Emma Strubell <emma.strubell@gmail.com>
2018-05-02 11:31:32 -0400

    debugging

--- network.py
+++ network.py
@@ -694,6 +694,10 @@ class Network(Configurable):
             predicate_indices = preds[:, 15:15+num_pred_srls]
           # print("predicate indices", predicate_indices)
           srl_preds_str = map(list, zip(*[self.convert_bilou(j) for j in np.transpose(srl_preds)]))
+          if not predicate_indices:
+            print("srl_preds_str", srl_preds_str)
+            print("srl_preds", srl_preds)
+            print("preds", preds)
           # print("srl_preds_str", srl_preds_str)
           for i, word in enumerate(words):
             pred = srl_preds_str[i] if srl_preds_str else []

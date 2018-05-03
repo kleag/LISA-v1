46f2e007bf66d2fcb9a961fbb1dd23a9920222b8
Emma Strubell <emma.strubell@gmail.com>
2018-05-03 13:14:26 -0400

    debug data

--- dataset.py
+++ dataset.py
@@ -181,7 +181,7 @@ class Dataset(Configurable):
             # print("new sent:", new_sent)
             # print("new preds:", map(lambda x: srls[int(x)], new_sent[:, -1]))
             tokens_str = ' '.join(word_part)
-            labels_str = ' '.join(map(lambda x: srls[x][0], correct_srls))
+            labels_str = ' '.join(map(lambda x: srls[x], correct_srls))
             # idx, tokens, labels
             print("%d %s ||| %s" % (p_idx, tokens_str, labels_str), file=tmp_f)
             examples += 1

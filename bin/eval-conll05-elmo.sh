4c5143ce93b289856e6bd38ea9fec727417512f3
Emma Strubell <emma.strubell@gmail.com>
2018-05-16 19:00:44 -0400

    bin

--- bin/eval-conll05-elmo.sh
+++ bin/eval-conll05-elmo.sh
@@ -9,17 +9,17 @@ data_dir=$DATA_DIR/conll05st-release-new
 
 valid_file=$data_dir/dev-set.gz.parse.sdeps.combined.bio
 valid_props_file=$data_dir/conll2005-dev-gold-props.txt
-valid_parse_file=$data_dir/dev-set.conll
+valid_parse_file=$data_dir/conll2005-dev-gold-parse.txt
 
 # WSJ test
 wsj_test_file=$data_dir/test.wsj.gz.parse.sdeps.combined.bio
 wsj_test_props_file=$data_dir/conll2005-test-wsj-gold-props.txt
-wsj_test_parse_file=$data_dir/test.wsj.conll
+wsj_test_parse_file=$data_dir/conll2005-test-wsj-gold-parse.txt
 
 # Brown test
 brown_test_file=$data_dir/test.brown.gz.parse.sdeps.combined.bio
 brown_test_props_file=$data_dir/conll2005-test-brown-gold-props.txt
-brown_test_parse_file=$data_dir/test.brown.conll
+brown_test_parse_file=$data_dir/conll2005-test-brown-gold-parse.txt
 
 # D&M injected
 dm_valid_file=lstm_predicted_parses_elmo/parse_preds-conll2005-lstm-dev24.tsv.replaced

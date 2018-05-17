f6df157585f45d59949f3c16aa88fcadea3635ce
Emma Strubell <emma.strubell@gmail.com>
2018-05-17 12:15:54 -0400

    cond?

--- lib/models/parsers/parser.py
+++ lib/models/parsers/parser.py
@@ -149,6 +149,8 @@ class Parser(BaseParser):
     else:
       use_gold_parse_tensor = tf.equal(int(use_gold_parse), 1)
 
+    self.print_once("use gold parse (%s): " % self.name, use_gold_parse)
+
     ##### Functions for predicting parse, Dozat-style #####
     def get_parse_logits(parse_inputs):
       if self.full_parse:

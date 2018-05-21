3376888db1d2dbd0fba084f04d54ca5bb81bc225
Emma Strubell <emma.strubell@gmail.com>
2018-05-21 08:34:18 -0400

    udpate

--- bin/eval-conll12-elmo.sh
+++ bin/eval-conll12-elmo.sh
@@ -17,8 +17,8 @@ test_props_file=$data_dir/conll2012-test-gold-props.txt
 test_parse_file=$data_dir/conll2012-test-gold-parse.txt
 
 # D&M injected
-dm_valid_file=lstm_predicted_parses_elmo/parse_preds-conll2012-lstm-dev.tsv.replaced
-dm_test_file=lstm_predicted_parses_elmo/parse_preds-conll2012-lstm-test.tsv.replaced
+dm_valid_file=lstm_predicted_parses_elmo/parse_preds-conll2012-lstm-dev-new.tsv.replaced
+dm_test_file=lstm_predicted_parses_elmo/parse_preds-conll2012-lstm-test-new.tsv.replaced
 
 if [[ "$parse_config" == "dm" || "$parse_config" == "gold" ]]; then
     gold_attn_at_train="False"
@@ -36,7 +36,6 @@ else
     inject_manual_attn="True"
 fi
 
-#    --test_eval \
 python $DOZAT_ROOT/network.py \
     --load \
     --test \
